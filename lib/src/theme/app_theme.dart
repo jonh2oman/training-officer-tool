40f6f2e73da20f7802abddbe1ecb7e4cb8476550
Jonathan Waterman <jonathanwaterman83@gmail.com>
2026-05-13 23:18:12 -0230

    Fix invalid const in app_theme.dart and downgrade google_fonts to 6.2.1 to resolve build errors

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -77,7 +77,7 @@ class AppTheme {
         bodyLarge: TextStyle(color: textPrimary),
         bodyMedium: TextStyle(color: textPrimary.withOpacity(0.7)),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: surface,
         elevation: 0,
         shape: RoundedRectangleBorder(
